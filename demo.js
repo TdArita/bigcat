7deffdaf1b81553f2bd49642ee132c78699bbe6f
TdArita <tang601619508@gmail.com>
2019-07-07 23:00:25 +0800

    leetcode submit

--- demo.js
+++ demo.js
@@ -48,3 +48,29 @@ function nth(list, index) {
   i++
   return nth(list.next, i)
 }
+
+
+function random() {
+  var twoNum = '' + rand(0.2) + rand(0.2)
+  if(twoNum == '10'){
+    return 1
+  }
+  if(twoNum == '01'){
+    return 0
+  }
+}
+function rand(p){
+  if (Math.random < p) {
+    return 1
+  }else{
+    return false
+  }
+}
+function sumri() {
+  var sum = {1:0,0:0}
+  for (let i = 0; i < 100; i++) {
+    var j = random()
+    sum[j]++
+  }
+}
+// 1 和 0 的概率还是50%
