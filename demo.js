cd31c987e59f099293bcf5a4d4eb19e0c275b523
TdArita <tang601619508@gmail.com>
2019-07-08 22:57:59 +0800

    leetcode submit

--- demo.js
+++ demo.js
@@ -4,8 +4,8 @@ function arrayToList(ary) {
   var List = {}
   if (ary[i])
   List[0] = i
-  List[1] = List[]
-  re
+  List[1] = List[0]
+  return
 }
 
 
@@ -74,3 +74,82 @@ function sumri() {
   }
 }
 // 1 和 0 的概率还是50%
+
+// 冒泡排序
+function bubbleSort(ary) {
+  for (let i = 0; i < ary.length - 1; i++) {
+    for (let j = 0; j < ary.length - 1 - i; j++) {
+      var left = 0
+      var right = 1
+      var temp = 0
+      if (ary[left] > ary [right]) {
+        temp = 
+      }
+    }
+  }
+}
+
+// 老师冒泡
+function swap(ary, i, j) {
+  if (i != j) {
+    var t = ary[i]
+    ary[i] = ary[j]
+    ary[j] = t
+  }
+}
+function bubbleSort(ary) {
+  for(var j = ary.length - 2; j >= 0; j--) {
+    var swapped = false
+    for(var i = 0; i <= j; i++) {
+      if (ary[i] > ary[i + 1]) {
+        swap(ary, i, i + 1)
+        swapped = true
+      }
+    }
+    if (!swapped) {
+      break
+    }
+  }
+  return ary
+}
+// 
+
+// 归并排序 Merge Sort
+function mergeSort(ary) {
+  var i = 0
+  var newAry = []
+  while (x) {
+    if ary1[i] >
+  }
+}
+
+function mergeSort(ary) {
+  if (ary.length < 2) {
+    return ary.slice()
+  }
+  var mid = ary.length >> 1
+  var left = ary.slice(0, mid)
+  var right = ary.slice(mid)
+
+  mergeSort(left)
+  mergeSort(right)
+
+  var i = 0
+  var j = 0
+  var k = 0
+
+  while(i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      ary[k++] = left[i++]
+    } else {
+      ary[k++] = right[j++]
+    }
+  }
+  while (i < left.length) {
+    ary[k++] = left[i++]
+  }
+  while (j < right.length) {
+    ary[k++] = right[j++]
+  }
+  return ary
+}
