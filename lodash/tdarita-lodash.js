15fdfa3f53bba0492e3a90decdb59a7d6d33b1c1
TdArita <tang601619508@gmail.com>
2019-07-10 20:51:27 +0800

    lodash second push

--- lodash/tdarita-lodash.js
+++ lodash/tdarita-lodash.js
@@ -1,5 +1,29 @@
-var tdarita = {
-  compact: function(ary) {
+var tdarita = function() {
+  function compact(ary) {
     return ary.filter(it => it)
-  },
-}
+  }
+
+  function concat(array,...args) {
+    for (let i = 0; i < args.length; i++) {
+      if(typeof(args[i]) == 'object' && args[i][0]){
+        for (let j = 0; j < args[i].length; j++) {
+          array.push(args[i][j])
+        }
+      }else{
+        array.push(args[i])
+      }
+    }
+    return array
+  }
+
+
+
+
+
+
+
+  return{
+    compact,
+    concat,
+  }
+}()
